b87a95e4d0b3ab41a5869fe8e9eca513fde4b5b1
Unknown <arifluthfi16@gmail.com>
2018-07-22 09:53:37 +0700

    Update

--- guru/index.php
+++ guru/index.php
@@ -4,6 +4,8 @@
     if(empty($_SESSION['level'] == 'Guru')) {
         header("location:../index.php");
     }
+
+    $conn = mysqli_connect("localhost","root","","bimbingankonseling");
 ?>
 
 <!DOCTYPE html>
@@ -35,8 +37,8 @@
         <sidebar>
             <div class="menu-sidebar">
                 <ul>
-                    <li class="dashboard active"><a href="">DashBoard</a></li>
-                    <li class="AddPelanggaran"><a href="">Tambah Pelanggaran</a></li>
+                    <li class="dashboard active"><a href="index.php">DashBoard</a></li>
+                    <li class="AddPelanggaran"><a href="tambah-pelanggaran.php">Tambah Pelanggaran</a></li>
                 </ul>
             </div>
         </sidebar>
@@ -45,17 +47,33 @@
             <main class="box-container">
                     <div class="jum siswa">
                         <p class="KetSiswa">Siswa</p>
-                        <p class="JumSiswa">10</p>
+                        <p class="JumSiswa">
+                        <?php 
+                        $sql = "SELECT * FROM t_murid";
+                        $hasil = mysqli_query($conn,$sql);
+                        $count = mysqli_num_rows($hasil);
+                        echo $count;
+                        ?></p>
                     </div>
 
                     <div class="jum guru">
                         <p class="KetGuru">Guru</p>
-                        <p class="JumGuru">10</p>    
+                        <p class="JumGuru"><?php 
+                        $sql   = "SELECT * FROM t_guru";
+                        $hasil = mysqli_query($conn,$sql);
+                        $count = mysqli_num_rows($hasil);
+                        echo $count;
+                        ?></p></p>    
                     </div>
 
                     <div class="jum pelanggaran">
                         <p class="KetPelanggaran">Pelanggaran</p>
-                        <p class="JumPelanggaran">10</p>
+                        <p class="JumPelanggaran"><?php 
+                        $sql = "SELECT * FROM t_pelanggaran";
+                        $hasil = mysqli_query($conn,$sql);
+                        $count = mysqli_num_rows($hasil);
+                        echo $count;
+                        ?></p></p>
                     </div>  
             </main>
             <main class="box-info">
---
+++ guru/tambah-pelanggaran.php
@@ -0,0 +1,100 @@
+<!-- cek apakah sudah login -->
+<?php
+session_start();
+if(empty($_SESSION['level'] == 'Guru')) {
+	header("location:../index.php");
+} 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bimbingan Konseling - Admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <script>
+        function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+        }
+    </script>
+</head>
+<body>
+    <header>
+            <h1>Bimbingan Konseling</h1>
+            <div class="box-profile">
+                <img src="" class="profile-img">
+                <p>Iffat Andriano</p>
+                <p>Admin</p>
+
+                <nav>
+                    <ul>
+                        <li><a href="">Logout</a></li>  
+                    </ul>
+                </nav>
+            </div>
+    </header>
+
+    <main class="container">
+        <sidebar>
+            <div class="menu-sidebar">
+                <ul>
+                    <li class="dashboard"><a href="index.php">DashBoard</a></li>
+                    <li class="AddSiswa"><a href="">Siswa</a></li>
+                    <div class="topnav">
+                    <!-- Navigation links (hidden by default) -->
+                    
+                    <li class="addData"><a href="#" onclick="myFunction()">Master Data</a></li>
+
+                    <div id="myLinks" class="myLinks">
+                        <a href="tampil-guru.php" class="active">Guru</a>
+                        <a href="#class">Kelas</a>
+                        <a href="#contact">Siswa</a>        
+                        <a href="#about">Wali Murid</a>
+                        <a href="#tatatertib">Pelanggaran</a>
+                    </div>
+                    <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+                    </div>             
+                </ul>
+            </div>
+        </sidebar>
+
+       <main class="box-info">
+            <div class="form">
+                <form class="add" action="proses-tambah-pelanggaran.php" method="POST">
+                    <h2 class="judulform">Input Form Pelanggaran</h2>
+                        <select name="nis">
+                            <?php
+                                $conn = mysqli_connect("localhost","root","","bimbingankonseling");
+                                $sql = "SELECT * FROM t_murid";
+                                $hasil = mysqli_query($conn,$sql);
+                                $row = mysqli_fetch_row($hasil);
+                                do{
+                                    list($nis)=$row;
+                                    echo "<option>$nis</option>";
+                                }while($row = mysqli_fetch_row($hasil));
+                            ?>
+                        </select>
+                        <input type="text" name="jenis" placeholder="Jenis Pelanggaran">
+                        <textarea name="keterangan" placeholder="Keterangan"></textarea>      
+                                 
+                    <input type="submit" value="Add Data" class="button">
+                </form>
+            </div>
+        </main>     
+        </main>
+        
+
+    </main>
+
+        
+    <footer>
+        Copyright &copy; 2018 <a href="">Bimbingan Konseling</a> All Right Reserved.
+    </footer>
+</body>
+</html>
